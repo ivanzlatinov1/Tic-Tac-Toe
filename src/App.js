3156d38efc6f126993b0026b3bac893a2d891cd6
Ivan Zlatinov <vani.zlatinov51@gmail.com>
2024-07-26 11:23:07 +0300

    Added game logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,85 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState  } from "react";
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(true);
+
+  function handleClick(i) 
+  {
+    if (squares[i] || calculateWinner(squares)) 
+    {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (isNext)
+    {
+       nextSquares[i] = 'X';
+    }
+    else 
+    {
+       nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setIsNext(!isNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner)
+  {
+    status = "Winner " + winner;
+  }
+  else
+  {
+    status = "Next player: " + (isNext ? "X" : "O");
+  }
+
+  return (
+  <>
+  <div className="status">{status}</div>
+  <div className="board-row">
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+  </div>
+  <div className="board-row">
+    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+  </div>
+  <div className="board-row">
+    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+  </div>
+  </>
+  );
+}
+
+function Square({ value, onSquareClick }) {
+  return  <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+function calculateWinner(squares) {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < winningLines.length; i++) 
+  {
+    const [a, b, c] = winningLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    {
+      return squares[a];
+    }
+  }
+  return null;
 }
